f811799512a404f5e8e5a675fa16227e2ee8db4e
PhanTienDat <tiendatbentre03@gmail.com>
2026-05-15 21:21:17 +0700

    Update testbench.sv

--- SYNCHRONOUS_FIFO/testbench.sv
+++ SYNCHRONOUS_FIFO/testbench.sv
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/14/2026 12:51:49 PM
-// Design Name: 
-// Module Name: testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module tb_FIFO;
